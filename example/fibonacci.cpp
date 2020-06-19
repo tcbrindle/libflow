91b1683d5edf835cb4d6bd67989254a4bc04bb56
Tristan Brindle <t.c.brindle@gmail.com>
2020-06-19 17:25:34 +0100

    Add a simple fibonacci generator example
    ...and a coro one as well for good measure

---
+++ example/fibonacci.cpp
@@ -0,0 +1,27 @@
+
+#include <flow.hpp>
+
+#include <iostream>
+
+struct fibonacci : flow::flow_base<fibonacci> {
+
+    explicit fibonacci(int a = 1, int b = 1) : a(a), b(b) {}
+
+    flow::maybe<int> next()
+    {
+        int temp = a;
+        a = b;
+        b = a + temp;
+        return temp;
+    }
+
+private:
+    int a;
+    int b;
+};
+
+int main()
+{
+    std::cout << "The first ten Fibonacci numbers are:\n";
+    fibonacci{}.take(10).write_to(std::cout, ", ");
+}
---
+++ example/fibonacci_coro.cpp
@@ -0,0 +1,29 @@
+
+#include <flow.hpp>
+
+#include <iostream>
+
+#ifdef FLOW_HAVE_COROUTINES
+
+flow::async<int> fibonacci(int a = 1, int b = 1)
+{
+    while (true) {
+        co_yield a;
+        std::tie(a, b) = std::tuple(b, a + b);
+    }
+}
+
+int main()
+{
+    std::cout << "The first ten Fibonacci numbers are:\n";
+    fibonacci().take(10).write_to(std::cout, ", ");
+}
+
+#else // !FLOW_HAVE_COROUTINES
+
+int main()
+{
+    std::cout << "This example requires coroutines support\n";
+}
+
+#endif
