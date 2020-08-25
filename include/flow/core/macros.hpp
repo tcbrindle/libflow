907d3afedd7d2458e78be2b5ce59cf0c6fad526a
Tristan Brindle <t.c.brindle@gmail.com>
2020-08-25 11:14:58 +0100

    Don't try to use coroutines with Clang+libstdc++
    Or rather, only #include <experimental/coroutine> when it's actually going to succeed

--- include/flow/core/macros.hpp
+++ include/flow/core/macros.hpp
@@ -2,6 +2,8 @@
 #ifndef FLOW_CORE_MACROS_HPP_INCLUDED
 #define FLOW_CORE_MACROS_HPP_INCLUDED
 
+#include <ciso646>
+
 #if defined(__has_cpp_attribute)
 #if __has_cpp_attribute(no_unique_address)
 #define FLOW_HAS_NO_UNIQUE_ADDRESS [[no_unique_address]]
@@ -15,8 +17,14 @@
 #endif
 
 #if defined(__cpp_coroutines)
-#define FLOW_HAVE_COROUTINES
-#endif
+#  if defined(__has_include) // If we can, check for <experimental/coroutine>
+#    if __has_include(<experimental/coroutine>)
+#      define FLOW_HAVE_COROUTINES
+#    endif // __cpp_coroutines
+#  else // can't check for it, let's hope for the best
+#    define FLOW_HAVE_COROUTINES
+#  endif // __has_include
+#endif // __cpp_coroutines
 
 #define FLOW_FWD(x) (static_cast<decltype(x)&&>(x))
 
