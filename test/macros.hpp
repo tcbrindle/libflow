9a5d60725c5b7bb54bdf4ae9b725d89216947eeb
Tristan Brindle <t.c.brindle@gmail.com>
2020-08-25 16:36:36 +0100

    Add install_libcxx.sh from Range-V3

--- test/macros.hpp
+++ test/macros.hpp
@@ -1,4 +1,6 @@
 
+#include <ciso646>
+
 #if defined(__GNUC__) && !defined(__clang__)
 #define COMPILER_IS_GCC 1
 #else
@@ -15,4 +17,13 @@
 #define COMPILER_IS_MSVC 1
 #else
 #define COMPILER_IS_MSVC 0
+#endif
+
+// If we're using a recent libstdc++, we'll assume we're using a recent-enough
+// GCC or Clang which can handle the constexpr tests
+#if defined(_GLIBCXX_RELEASE) && _GLIBCXX_RELEASE >= 9
+#define HAVE_CONSTEXPR_TESTS 1
+// Otherwise,
+#elif defined(_LIBCPP_VERSION) && _LIBCPP_VERSION >= 8000
+#define HAVE_CONSTEXPR_TESTS 1
 #endif
