3cc9f86b75935f8d596b971369fc4026d7c613ee
Tristan Brindle <t.c.brindle@gmail.com>
2020-08-25 16:54:36 +0100

    Stop CTest failing when we don't have coroutines

--- test/test_async.cpp
+++ test/test_async.cpp
@@ -1,27 +1,32 @@
 
 #include <flow.hpp>
 
-#ifdef FLOW_HAVE_COROUTINES
-
 #include "catch.hpp"
 
-#include <iostream>
+// This file is a bit of a mess because the regex used to add Catch tests to
+// CTest doesn't look inside ifdefs, so we need to ensure the tests are always
+// present (and will trivially pass) even when we don't have coro support.
 
 namespace {
 
-auto get_names() -> flow::async<std::string> {
+#ifdef FLOW_HAVE_COROUTINES
+
+auto get_names() -> flow::async<std::string>
+{
     co_yield "Adam";
     co_yield "Barbara";
     co_yield "Clive";
 }
 
-auto ints(int from = 0) -> flow::async<int> {
+auto ints(int from = 0) -> flow::async<int>
+{
     while (true) {
         co_yield from++;
     }
 }
 
-auto ints(int from, int to) -> flow::async<int> {
+auto ints(int from, int to) -> flow::async<int>
+{
     while (from < to) {
         co_yield from++;
     }
@@ -31,10 +36,13 @@ using triple = std::tuple<int, int, int>;
 
 auto pythagorean_triples() -> flow::async<triple>
 {
-    FLOW_FOR (int z, ::ints(1)) {
-        FLOW_FOR (int y, ::ints(1, z)) {
-            FLOW_FOR (int x, ::ints(1, y)) {
-                if (x*x + y*y == z*z) {
+    FLOW_FOR(int z, ::ints(1))
+    {
+        FLOW_FOR(int y, ::ints(1, z))
+        {
+            FLOW_FOR(int x, ::ints(1, y))
+            {
+                if (x * x + y * y == z * z) {
                     co_yield {x, y, z};
                 }
             }
@@ -42,34 +50,39 @@ auto pythagorean_triples() -> flow::async<triple>
     }
 }
 
-}
+#endif // FLOW_HAVE_COROUTINES
 
 TEST_CASE("async names", "[flow.async]")
 {
+#ifdef FLOW_HAVE_COROUTINES
     std::vector<std::string> vec = get_names().collect();
 
     REQUIRE(vec.size() == 3);
     REQUIRE((vec == std::vector<std::string>{"Adam", "Barbara", "Clive"}));
 
     REQUIRE(get_names().sum() == "AdamBarbaraClive");
+#endif
 }
 
-TEST_CASE("all the ints", "[flow.async]")
+TEST_CASE("async ints", "[flow.async]")
 {
+#ifdef FLOW_HAVE_COROUTINES
     auto vec = ints().take(5).to_vector();
 
     REQUIRE(vec.size() == 5);
     for (std::size_t i = 0; i < vec.size(); i++) {
         REQUIRE(vec[i] == i);
     }
+#endif
 }
 
-TEST_CASE("Triples", "[flow.async]")
+TEST_CASE("async triples", "[flow.async]")
 {
+#ifdef FLOW_HAVE_COROUTINES
     for (auto [x, y, z] : pythagorean_triples().take(10).to_range()) {
-        REQUIRE(x*x + y*y == z*z);
+        REQUIRE(x * x + y * y == z * z);
     }
+#endif
 }
 
-
-#endif // FLOW_HAVE_COROUTINES
+}
