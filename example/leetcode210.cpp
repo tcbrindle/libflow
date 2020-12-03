da712635976dd1dfbac0974ddb1c44dd69dcd282
Tristan Brindle <t.c.brindle@gmail.com>
2020-12-03 23:47:22 +0000

    Add another leetcode example
    ...from another of Conor's videos

---
+++ example/leetcode210.cpp
@@ -0,0 +1,39 @@
+
+#include <flow.hpp>
+
+// Taken from "1 Problem, 8 Programming Languages" YouTube video by
+// Conor Hoekstra (@code_report)
+// https://www.youtube.com/watch?v=zrOIQEN3Wkk
+
+// Original Leetcode contest problem:
+// https://leetcode.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
+
+constexpr auto max_depth = [](std::string_view const input) {
+    return flow::filter(input, flow::pred::in('(', ')'))
+        .map([](char c) { return c == '(' ? 1 : -1; })
+        .partial_sum()
+        .max().value_or(0);
+};
+
+int main()
+{
+    {
+        constexpr auto in = "(1+(2*3)+((8)/4))+1";
+        static_assert(max_depth(in) == 3);
+    }
+
+    {
+        constexpr auto in = "(1)+((2))+(((3)))";
+        static_assert(max_depth(in) == 3);
+    }
+
+    {
+        constexpr auto in = "1+(2*3)/(2-1)";
+        static_assert(max_depth(in) == 1);
+    }
+
+    {
+        constexpr auto in = "1";
+        static_assert(max_depth(in) == 0);
+    }
+}
