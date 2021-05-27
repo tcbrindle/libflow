555a4ca118f370f669800ca58036fdb61ff1c9c5
Tristan Brindle <t.c.brindle@gmail.com>
2021-05-27 19:31:46 +0100

    Add cartesian_product tests

--- test/test_cartesian_product.cpp
+++ test/test_cartesian_product.cpp
@@ -4,18 +4,46 @@
 #include <flow.hpp>
 #include <iostream>
 
+constexpr bool test_cartesian_product3()
+{
+    auto const arr = std::array{10, 20};
+    auto ints = flow::ints().take(2);
+    std::string_view strs[] = { "one", "two"};
+
+    auto prod = flow::cartesian_product(arr, std::move(ints), strs);
+
+    using output_t = std::tuple<int const&, flow::dist_t, std::string_view&>;
+
+    static_assert(std::is_same_v<flow::item_t<decltype(prod)>, output_t>);
+
+    const auto output = std::array<std::tuple<int, flow::dist_t, std::string_view>, 8>{
+        std::tuple{10, 0, "one"}, {10, 0, "two"},
+        {10, 1, "one"}, {10, 1, "two"},
+        {20, 0, "one"}, {20, 0, "two"},
+        {20, 1, "one"}, {20, 1, "two"}
+    };
+
+    return prod.size() == 2 * 2 * 2 &&
+            flow::equal(std::move(prod), output);
+}
+static_assert(test_cartesian_product3());
+
+constexpr bool test_cartesian_product2()
+{
+    return flow::of{1, 2, 3}
+                    .cartesian_product(flow::c_str("abc"))
+                    .map([](auto p) {
+                        auto [i, c] = p;
+                        return flow::of(c).cycle().take(i);
+                    })
+                    .flatten()
+                    .equal(flow::c_str("abcaabbccaaabbbccc"));
+}
+static_assert(test_cartesian_product2());
+
 TEST_CASE("cartesian_product()")
 {
-    auto const vec1 = std::vector{1, 2, 3};
-    auto const vec2 = std::vector{1.f, 2.f, 3.f};
-    auto plus_point_1 = [](auto x) { return x + 0.1; };
+    REQUIRE(test_cartesian_product3());
 
-    auto prod = flow::cartesian_product(vec1, vec2, flow::map(vec2, plus_point_1));
-
-    static_assert(std::is_same_v<flow::item_t<decltype(prod)>, std::tuple<int const&, float const&, double>>);
-
-    std::move(prod).for_each([](auto p) {
-        auto const& [x, y, z] = p;
-        std::cout << x << ' ' << y << ' ' << z << '\n';
-    });
+    REQUIRE(test_cartesian_product2());
 }
